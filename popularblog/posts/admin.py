da465a726e0f31ddcbfb7f8072d88d516d9668d3
Засолкин Евгений <men6586@yandex.ru>
2021-05-03 19:06:46 +0300

    create models, migrate, user

---
+++ popularblog/posts/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    list_filter = ('pub_date',)
+    empty_value_display = "-пусто-"
+admin.site.register(Post, PostAdmin)
