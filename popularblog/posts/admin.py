6d3752f6a94d677382746f2e7a97862b8afa6173
Засолкин Евгений <men6586@yandex.ru>
2021-05-07 18:52:25 +0300

    etst

--- popularblog/posts/admin.py
+++ popularblog/posts/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('text', 'pub_date', 'author')
     list_filter = ('pub_date',)
     empty_value_display = "-пусто-"
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title',)
+    empty_value_display = "-пусто-"
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
